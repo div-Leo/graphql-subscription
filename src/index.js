f7b3143c65ccd7c9c0f9bb24632f6d6d21a7ea3c
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2020-03-19 15:12:20 +0100

    fix: apollo srver bug

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const { GraphQLServer, PubSub } = require("apollo-server");
+const { ApolloServer, PubSub } = require("apollo-server");
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 const pubsub = new PubSub();
 
-const server = new GraphQLServer({
+const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: {
